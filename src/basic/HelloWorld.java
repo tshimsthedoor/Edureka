209166a1369e3ad4362b39e384b0c3b7b19a86bb
tshimsthedoor <tshims79@gmail.com>
2023-09-10 21:16:09 +0200

    I added a system out print

--- src/basic/HelloWorld.java
+++ src/basic/HelloWorld.java
@@ -3,7 +3,8 @@ package basic;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Tresor DevOps");
+		System.out.println("Hello Tresor DevOps, welcome to this journay");
+		System.out.println("You will be your own boss");
 
 	}
 
